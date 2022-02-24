00569e47f5eaed81c34c6b139e02a0442dff2e69
Abdliswi <engabdliswi@gmail.com>
2022-02-24 08:29:50 +0200

    TASK11 resubmitt

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
-'use strict';
-
-const express = require("express");
-
+const express = require('express');
+const data = require('./MovieData/data.json') // path of data
 const app = express();
+const port = 3000;
 
-app.get ('/', hellpworldHandler);
+function MovieData(title, poster_path, overview,){
+    this.title = title;
+    this.poster_path = poster_path;
+    this.overview = overview;
+};
 
-function hellpworldHandler(request, response) {
-        console.log(request);
-}
-
-app.listen(3000 , () => {
-
-    console.log("listen on 3000")
-    
+app.get('/', (req, res) => { //
+    let result = [];
+    data.data.forEach((value) => {
+        let oneMovie = new MovieData(value.title,value.poster_path, value.overview);
+        result.push(oneMovie);
+    });
+    return res.json(result);
 });
 
 
 
+app.get('/favorite', (req, res) => {    
+    res.json("Welcome to Favorite Page");
+//    res.send("Welcome to Favorite Page");
+
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
 
